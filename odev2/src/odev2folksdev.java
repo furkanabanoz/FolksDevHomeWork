ab0d722b11c131930b28c08d4dc4312717e92d01
Furkan <furkan-abanoz@outlook.com>
2021-09-29 21:41:09 +0300

    java commit

--- odev2/src/odev2folksdev.java
+++ odev2/src/odev2folksdev.java
@@ -1,3 +1,17 @@
 public class odev2folksdev {
+    public static void main(String[] args) {
+        for (int i = 1; i <=6 ; i++) {
+
+            for (int k = 1; k <=6-i ; k++) {
+                System.out.printf(" ");
+            }
+
+            for (int j = 1; j <=i ; j++) {
+                System.out.printf(String.valueOf(j));
+            }
+            System.out.printf("\n");
+            
+        }
+    }
 
 }
