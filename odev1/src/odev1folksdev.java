facec71601e4d3deccffbb144f96110fb19d680f
Furkan <furkan-abanoz@outlook.com>
2021-09-29 21:15:13 +0300

    java commit

---
+++ odev1/src/odev1folksdev.java
@@ -0,0 +1,12 @@
+public class odev1folksdev {
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.printf("*");
+
+
+            }
+            System.out.printf("\n");
+        }
+    }
+}
---
+++ odev2/src/odev2folksdev.java
@@ -0,0 +1,3 @@
+public class odev2folksdev {
+
+}
